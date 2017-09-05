c7ae6909c72a295bd14cf7acd90f0a39f73037ed
artemyarotskyi <artemyarotskyi@gmail.com>
2017-09-05 17:32:11 +0300

    (Update)Fixed adding event to stack after moving element(when move to borders)

--- CommonUiControllers/Lamp.cpp
+++ CommonUiControllers/Lamp.cpp
@@ -87,7 +87,7 @@ void Lamp::mouseReleaseEvent(QGraphicsSceneMouseEvent *event)
 {
     QAbstractGraphicsShapeItem::mouseReleaseEvent(event);
 
-    if((mOldX != mX) && (mOldY != mY))
+    if((mOldX != mX) || (mOldY != mY))
         emit lampMove(this);
 
     if(mOldAngle != mAngle)
@@ -110,10 +110,10 @@ void Lamp::mouseMoveEvent(QGraphicsSceneMouseEvent *event)
             {
                 QAbstractGraphicsShapeItem::mouseMoveEvent(event);
             }
-            if(this->x() < 0) this->setX(2);
-            if(this->y() < 0) this->setY(2);
-            if(this->x() + 53 > 600) this->setX(600 - 55);
-            if(this->y() + 53 > 520) this->setY(520 - 55);
+            if(this->x() < 0) this->setX(1);
+            if(this->y() < 0) this->setY(1);
+            if(this->x() + 53 > 600) this->setX(600 - 54);
+            if(this->y() + 53 > 520) this->setY(520 - 54);
 
             mX = this->x();
             mY = this->y();
